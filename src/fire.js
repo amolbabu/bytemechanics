6f81b13f9e3ff8693d25841d405f2089bb8a6c07
saysun <sunayong@gmail.com>
2018-11-17 19:33:12 +0800

    added firebase connections and test

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyCEL2bTrEF54b0oJDrCSCSE0PasYMNE0TQ",
+    authDomain: "collective-intelligence-f2bb1.firebaseapp.com",
+    databaseURL: "https://collective-intelligence-f2bb1.firebaseio.com",
+    projectId: "collective-intelligence-f2bb1",
+    storageBucket: "collective-intelligence-f2bb1.appspot.com",
+    messagingSenderId: "519244062558"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
