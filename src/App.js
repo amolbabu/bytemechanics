10f98d810dcab026eeacebb1e8e89906063617a1
amolbabu <amolbabu@gmail.com>
2018-11-17 20:51:42 +0800

    Added Ask page content

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  Vote from './Vote.js';
+import Ask from './Ask.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React4
-          </a>
-          <Vote/>
+            <label className="App-logo" alt="logo">NAO</label>
+          Create your Questions!
+
+          <Ask/>
         </header>
       </div>
     );
