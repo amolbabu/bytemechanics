e9dadf22544977b5ace03aa3cc4ee48dda36196b
amolbabu <amolbabu@gmail.com>
2018-11-19 13:51:55 +0800

    Registration and Login connected to firebase. Added error message display

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import firebase from "../fire";
 
-class Login extends Component{
+class Login extends Component {
 
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailId: this.emailId,
+            password: this.pwd,
+            error: this.error
+        }
+    }
+
+    updateEmailState(value) {
+        this.setState({emailId: value});
+    }
+
+    updatePwdState(value) {
+        this.setState({pwd: value});
+    }
+
+    setError(value) {
+       this.setState({error: value});
+    }
+
+
+
+
+    authenticateUser(emailId, password) {
+        return firebase.auth().signInWithEmailAndPassword(emailId, password).catch((error)=>{
+            this.setError(error.message)
+        });
+
+    }
+
+
+    render() {
+        const errors = this.state.error
         return (
             <body>
 
             <div className="container">
                 <h1 align="center">Welcome to Nao</h1>
-                    <div className="row">
-                        <div className="vl">
-                            <span className="vl-innertext">or</span>
-                        </div>
 
 
-                        <div className="col">
-                            <div className="hide-md-lg">
-                                <p>Or sign in manually:</p>
-                            </div>
-
-                            <input type="text" name="username" placeholder="Username" required/>
-                                <input type="password" name="password" placeholder="Password" required/>
-                                    <input type="submit" value="Login"/>
-                        </div>
-                        <div className="col">
-                            <input type="button" value="Login as Guest"/>
-                        </div>
-
+                <ul className="errors"><strong><font color="red">{errors}</font></strong></ul>
+                <div className="row">
+                    <div className="vl">
+                        <span className="vl-innertext">or</span>
                     </div>
+
+
+                    <div className="col">
+                        <div className="hide-md-lg">
+                            <p>Or sign in manually:</p>
+                        </div>
+
+                        <input type="text" name="username" placeholder="Username" required
+                               onChange={(evt)=>this.updateEmailState(evt.target.value)}/>
+                        <input type="password" name="password" placeholder="Password" required
+                               onChange={(evt)=>this.updatePwdState(evt.target.value)}/>
+                        <input type="submit" value="Login" onClick={(e) => (this.authenticateUser(this.state.emailId, this.state.pwd))}/>
+                    </div>
+                    <div className="col">
+                        <input type="button" value="Login as Guest"/>
+                    </div>
+
+                </div>
 
             </div>
 
@@ -40,4 +79,5 @@ class Login extends Component{
         );
     }
 }
+
 export default Login;
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,6 +1,39 @@
 import React, {Component} from 'react';
+import firebase from "../fire";
 
 class Registration extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailId: this.emailId,
+            password: this.pwd
+
+        }
+    }
+
+    updateEmailState(value) {
+        this.setState({emailId: value});
+    }
+
+    updatePwdState(value) {
+        this.setState({pwd: value});
+    }
+
+
+    registerUser(email, password) {
+        console.log("Inside register User");
+        console.log("emailID " + email);
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+    }
+
+
     render() {
         return (
             <div className="container">
@@ -10,15 +43,21 @@ class Registration extends Component {
                 <label htmlFor="text"><b>Name</b></label>
                 <input type="text" placeholder="Enter Name" name="name" id='name' required/>
                 <label htmlFor="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required/>
+                <input type="text" placeholder="Enter Email" name="emailId" id='emailId' required
+                       onChange={(evt)=>this.updateEmailState(evt.target.value)}/>
                 <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required/>
+                <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required
+                       onChange={(evt)=>this.updatePwdState(evt.target.value)}/>
                 <label htmlFor="psw-repeat"><b>Confirm Password</b></label>
                 <input type="password" placeholder="Confirm Password" name="confirmpwd" required/>
                 <hr/>
                 <p>By creating an account you agree to Credit Suisse <a href="#">Terms & Privacy</a>.</p>
 
-                <button type="submit" className="registerbtn">Register</button>
+
+                <button onClick={(e) => (this.registerUser(this.state.emailId, this.state.pwd))}>
+                    Sign-Up
+                </button>
+
                 <hr/>
                 <div className="container signin">
                     <p>Already have an account? <a href="/login">Sign in</a>.</p>
@@ -27,4 +66,5 @@ class Registration extends Component {
         );
     }
 }
+
 export default Registration
